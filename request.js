1f87fb4d4be22032e4e25955b8c1aa1cfaab3e3d
IA-n7 <ian.e.w.bradley@gmail.com>
2018-06-11 14:23:23 +0000

    Added Html request file/function, prints data

---
+++ request.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+https.get(requestOptions, function (response) {
+
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    console.log("All the data: " + data);
+  });
+
+  response.on('end', function() {
+    console.log('End of all data/chunks');
+  });
+
+});
+}
+
